d0602428e31e599903e97fa400692b3e90149e8a
PLUCHARD MAXIMILIEN <mpluchar@localhost.localdomain>
2018-10-05 16:15:59 +0200

    jai un halo joli

--- GLImac-Template/TP2/shaders/halo.fs.glsl
+++ GLImac-Template/TP2/shaders/halo.fs.glsl
@@ -9,13 +9,13 @@ void main() {
   
   
   float dist = distance(vFragPosition, vec2(0.0, 0.0));
-  float alpha = 1.2;
-  float beta = 5.0;
+  float alpha = 3.1;
+  float beta = 25.0;
   float sqrtDistBeta = dist * dist * (-beta);
   float coeff = alpha * exp(sqrtDistBeta);
   /*
-  float coeff2 = alpha * dist;
-  float coeff = 1 / (1.0 + coeff1 + coeff2);
-*/
+    float coeff2 = alpha * dist;
+    float coeff = 1 / (1.0 + coeff1 + coeff2);
+  */
   fFragColor = coeff * vFragColor;
 };
